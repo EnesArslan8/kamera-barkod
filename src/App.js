2d4de15e980fcbeb5b1f4734dc42f552681e47c8
EnesArslan8 <enes8arslan@gmail.com>
2023-07-13 17:51:54 +0300

    User's cam can read barcode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import "./App.css";
 import { useEffect, useRef } from "react";
 
-
 function App() {
   const video = useRef(null);
-  useEffect(() => {
+  const canvas = useRef(null);
+
+  const openCam = () => {
     navigator.mediaDevices
       .getUserMedia({ video: { width: 1280, height: 720 } })
       .then((stream) => {
-        video.current.srcObject=stream;
+        video.current.srcObject = stream;
+        video.current.play();
+
+        const ctx = canvas.current.getContext("2d");
+
+        const barcodeDetector = new window.BarcodeDetector({
+          formats: ["ean_13", "qr_code"]
+        });
+        setInterval(() => {
+          canvas.current.width = video.current.videoWidth;
+          canvas.current.height = video.current.videoHeight;
+          ctx.drawImage(
+            video.current,
+            0,
+            0,
+            video.current.videoWidth,
+            video.current.videoHeight
+          );
+          barcodeDetector
+            .detect(canvas.current)
+            .then((barcodes) => {
+              barcodes.forEach((barcode) => console.log(barcode.rawValue));
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }, 100);
       })
       .catch((err) => {
-        console.log("Hata meydana geldi"+ err);
+        console.log("Hata meydana geldi" + err);
       });
-  }, []);
+  };
 
   return (
     <div className="App">
-      <video ref={video} autoPlay muted></video>
+      <div>
+        <video ref={video} autoPlay muted hidden></video>
+        <canvas ref={canvas}></canvas>
+      </div>
+      <button onClick={openCam}>Kamerayı Aç</button>
     </div>
   );
 }
